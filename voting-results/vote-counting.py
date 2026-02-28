79b2e6aef27280ab9f3536a3a712e859d938d09d
TheMartuu <damnedmartuu24@gmail.com>
2026-02-28 19:45:13 -0300

    added script and json samples

---
+++ voting-results/vote-counting.py
@@ -0,0 +1,97 @@
+import json 
+from pathlib import Path 
+
+candidates_file = Path('candidates.json')
+people_ids_file = Path('people_ids_file.json')
+
+def load_ids():
+    """loads file with people's ids if exists or creates another one if not"""
+    if people_ids_file.exists():
+        with open(people_ids_file,'r',encoding="utf-8") as f: 
+            try: 
+                return set(json.load(f))
+            except json.JSONDecodeError:
+                return set()
+    return set()
+
+def save_new_id(new_id): 
+    """saves new ID"""
+    with open (people_ids_file,'w',encoding="utf-8") as f: 
+        json.dump(list(new_id),f,ensure_ascii=False,indent=2)
+
+def save_candidate(candidates_list): 
+    """saves new vote"""
+    with open (candidates_file,'w',encoding="utf-8") as f: 
+        json.dump(candidates_list,f,ensure_ascii=False,indent=2)
+
+def load_candidates_list(): 
+    """loads file with candidates if exists or creates another one if not"""
+    if candidates_file.exists():
+        with open(candidates_file,'r',encoding="utf-8") as f: 
+            try: 
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    return []
+
+
+def get_id(new_voter_id):
+    """Validates IDs before voting"""
+    voter_ids_list = load_ids()
+    if len(new_voter_id) ==  8: 
+        if new_voter_id not in voter_ids_list: 
+            voter_ids_list.add(new_voter_id)
+            save_new_id(voter_ids_list)
+            return True
+        else: 
+            print("That ID is already registered, you cannot vote twice!")
+            return False 
+    else:
+        print("ID not valid!")
+        return False
+
+def register_vote(candidate): 
+    """Registers votes in the json file"""
+    candidates_list = load_candidates_list()
+    for register in candidates_list:
+            register['name'] == candidate
+            register['votes'] += 1
+            save_candidate(candidates_list)
+            print("Vote registered!")
+            return 
+    print("Candidate not found!")
+
+def show_total_results():
+    """Reads json file and displays total votes"""
+    candidates_list = load_candidates_list()
+    for candidate in candidates_list:
+        print(f"{candidate['name']}: has {candidate['votes']} votes.")
+
+def show_winner():
+    """Displays the most voted candidate"""
+    candidates_list = load_candidates_list()
+    winner = (max(candidates_list, key=lambda c: c["votes"]))
+    print(f"The winner is {winner['name']} with {winner['votes']} votes!")
+
+
+                
+
+active = True
+menu_options = 'Welcome to the poll.'
+while active: 
+    menu_options = str(input('Please enter options: \n1. Register vote \n2. Read results \n3. Check winner\n4. Exit \n'))
+    if menu_options == '1':
+        prompt_new_id = input("Please enter your ID: ")
+        if get_id(prompt_new_id): 
+            prompt_new_vote = input("Please enter your candidate: ")
+            register_vote(prompt_new_vote)
+    elif menu_options == '2':
+        show_total_results()
+    elif menu_options == '3':
+        show_winner()
+    elif menu_options == '4':
+        print('Bye!')
+        active = False
+    else:
+        print('Option not valid!')
+    
