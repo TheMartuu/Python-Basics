19b0221dc9e63eefc4aa087464bd81b838bac2c3
TheMartuu <damnedmartuu24@gmail.com>
2026-03-03 00:06:42 -0300

    added init line

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -2,6 +2,7 @@ from collections import Counter
 from pathlib import Path
 
 #Wuthering Heights text taken from https://gutenberg.org
+
 file = Path('wuthering-heights.txt')
 
 def load_text():
@@ -15,10 +16,12 @@ def count_words(text):
     return Counter(words)
 
 def top_ten_words(counter): 
+    """returns the first most repeated words in the text"""
     return counter.most_common(10)
         
 
-text_content = load_text()
-counting_words = count_words(text_content)
-for word, count in top_ten_words(counting_words):
-    print (f"The word '{word}' is repeated {count} times. ")
+if __name__ == "__main__":
+    text_content = load_text()
+    counting_words = count_words(text_content)
+    for word, count in top_ten_words(counting_words):
+        print(f"The word '{word}' is repeated {count} times.")
